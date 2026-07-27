83a9614cc6f35385e5592773ec314337814167df
Acer <Acer@DESKTOP-91FJ23N>
2026-07-27 22:43:54 +0530

    commitbyShankar

--- TestGitRepo/src/com/demo/Shankar.java
+++ TestGitRepo/src/com/demo/Shankar.java
@@ -3,6 +3,6 @@ package com.demo;
 public class Shankar {
 
 	public static void main(String[] args) {
-		System.out.println("Shanakr from group  A");
+		System.out.println("Shanakr  from group  A");
 	}
 }
