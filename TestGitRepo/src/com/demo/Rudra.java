ea262ca84d5e859dd863102b8143afdcc9335f5d
rudra-parmeshwar <joddy4824@gmail.com>
2026-07-28 21:22:24 +0530

    Commiting to master_rudra (#3)

--- TestGitRepo/src/com/demo/Rudra.java
+++ TestGitRepo/src/com/demo/Rudra.java
@@ -5,6 +5,7 @@ public class Rudra {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Rudra Commit");
+		System.out.println("Commiting to master-Rudra branch");
 
 	}
 
