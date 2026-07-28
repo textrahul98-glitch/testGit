76a52fabaf2cfc881120b2e4c010785ed6abd918
Shankar Narwade <shankarnarwade209@gmail.com>
2026-07-28 21:33:36 +0530

    Added Even odd program (#5)
    Co-authored-by: Acer <Acer@DESKTOP-91FJ23N>

--- TestGitRepo/src/com/demo/Shankar.java
+++ TestGitRepo/src/com/demo/Shankar.java
@@ -1,8 +1,22 @@
 package com.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shankar {
 
 	public static void main(String[] args) {
-		System.out.println("Shanakr  from group  A");
+		public static void main(String[] args) {
+			List<Integer> list = new ArrayList<>();
+			list.add(12);
+			list.add(20);
+			list.add(37);
+			list.add(15);
+			list.add(5);
+			list.add(42);
+			System.out.println(list);
+			list.stream().filter(x->x%2==0).forEach(p->System.out.println(p));
+			
+		}
 	}
 }
