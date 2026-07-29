85f256fa142339f5ff197a2c583ac89f79cc1f87
Acer <Acer@DESKTOP-91FJ23N>
2026-07-29 13:00:19 +0530

    final commit

--- TestGitRepo/src/com/demo/Shankar.java
+++ TestGitRepo/src/com/demo/Shankar.java
@@ -1,8 +1,12 @@
 package com.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Shankar {
 
-	public static void main(String[] args) {
-		System.out.println("Shanakr  from group  A");
-	}
+	 public static void main(String[] args) {
+	    List<Integer> list = Arrays.asList(10,5,20,15,16,12,30);
+	    list.stream().filter(x-> x%2 ==0).forEach(p->System.out.println(p));
+	  }
 }
