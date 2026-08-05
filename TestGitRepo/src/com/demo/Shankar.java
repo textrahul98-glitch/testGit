79c80f7458281689fb2e652ace38f905e6336171
shankarnarwade09-crypto <shankarnarwade209@gmail.com>
2026-08-05 13:21:52 +0530

    newly added

--- TestGitRepo/src/com/demo/Shankar.java
+++ TestGitRepo/src/com/demo/Shankar.java
@@ -7,7 +7,6 @@ public class Shankar {
 
 	 public static void main(String[] args) {
 	    List<Integer> list = Arrays.asList(10,5,20,15,16,12,30);
-	    
 	    list.stream().filter(x-> x%2 ==0).forEach(p->System.out.println(p));
 	  }
 }
